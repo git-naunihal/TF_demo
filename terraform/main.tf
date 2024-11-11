eb593fad30d8ec58db42f727394865ee874c0e08
Nishu Singh <104611378+codewithnishu@users.noreply.github.com>
2024-11-11 19:24:17 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,23 @@
-provider “aws” {
-  region = “eu-west-1”
-}
-
 terraform {
-  backend "s3" {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+ backend "s3" {
     bucket         = "tf.lab"
     key            = "terraform.tfstate"
     region         = "eu-west-1"
   }
+
 }
+
+
+provider "aws" {
+ region = "us-east-1"
+}
+
 
 resource "aws_instance" "test_instance" {
  ami           = "ami-830c94e3"
