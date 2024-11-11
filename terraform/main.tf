2df2b4709b5cc5041ba5b7189565ed0d7e3c952c
Nishu Singh <104611378+codewithnishu@users.noreply.github.com>
2024-11-11 20:36:22 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,24 +5,24 @@ terraform {
     }
   }
 
- backend "s3" {
-    bucket         = "tf.lab"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
+  backend "s3" {
+    bucket = "tf.lab"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
   }
 
 }
 
 
 provider "aws" {
- region = "us-east-1"
+  region = "us-east-1"
 }
 
 
 resource "aws_instance" "test_instance" {
- ami           = "ami-063d43db0594b521b"
- instance_type = "t2.nano"
- tags = {
-   Name = "test_instance"
- }
+  ami           = "ami-063d43db0594b521b"
+  instance_type = "t2.nano"
+  tags = {
+    Name = "test_instance"
+  }
 }
