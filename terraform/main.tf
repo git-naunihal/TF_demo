f55b3ccf633cb70fb00307c3f1f2273cb68ec3ef
Nishu Singh <104611378+codewithnishu@users.noreply.github.com>
2024-11-11 20:23:20 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,6 @@ terraform {
     bucket         = "tf.lab"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    profile        = "tfadmin"
   }
 
 }
@@ -21,7 +20,7 @@ provider "aws" {
 
 
 resource "aws_instance" "test_instance" {
- ami           = "ami-0b72821e2f351e396"
+ ami           = "ami-063d43db0594b521b"
  instance_type = "t2.micro"
  tags = {
    Name = "test_instance-GitHub-Action"
